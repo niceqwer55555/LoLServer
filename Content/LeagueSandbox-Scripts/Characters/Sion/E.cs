5c28f8a11831813a58e6d514467331cbb91f9f1d
phillipchain <knights.phillip.chain@outlook.com>
2024-06-23 23:32:50 +0200

    [Add/Character] Placeholder Sion

--- Content/LeagueSandbox-Scripts/Characters/Sion/E.cs
+++ Content/LeagueSandbox-Scripts/Characters/Sion/E.cs
@@ -10,14 +10,15 @@ using GameServerCore.Enums;
 using LeagueSandbox.GameServer.GameObjects.SpellNS.Missile;
 using LeagueSandbox.GameServer.GameObjects.SpellNS.Sector;
 using LeagueSandbox.GameServer.API;
+using GameServerLib.GameObjects.AttackableUnits;
 
 namespace Spells
 {
-    public class SionW : ISpellScript
+    public class SionE : ISpellScript
     {
         ObjAIBase Owner;
-
-        Vector2 spellpos;
+        float timeSinceLastTick = 1000f;
+        Spell Spell;
         public SpellScriptMetadata ScriptMetadata { get; private set; } = new SpellScriptMetadata()
         {
             TriggersSpellCasts = true,
@@ -26,15 +27,26 @@ namespace Spells
         public void OnActivate(ObjAIBase owner, Spell spell)
         {
             Owner = owner;
+            Spell = spell;
+            ApiEventManager.OnKillUnit.AddListener(this, owner, OnKillUnit, false);
+        }
+        public void OnKillUnit(DeathData data)
+        {
+            AddBuff("SionEnrageHP", 1, 1, Spell, Owner, Owner, true);
         }
 
-
-        public void OnSpellCast(Spell spell)
+        public void OnSpellPostCast(Spell spell)
         {
-            var owner = spell.CastInfo.Owner;
-
-            AddBuff("SionWShield", 10f, 1, spell, Owner, Owner, false);
-            AddBuff("SionWSwitch", 10f, 1, spell, Owner, Owner, false);
+            if (!Owner.HasBuff("SionEnrage"))
+            {
+                AddBuff("SionEnrage", 1, 1, spell, Owner, Owner, true);
+                timeSinceLastTick = 1000f;
+                spell.SetCooldown(0.5f, true);
+            }
+            else
+            {
+                RemoveBuff(Owner, "SionEnrage");
+            }
         }
     }
 }
