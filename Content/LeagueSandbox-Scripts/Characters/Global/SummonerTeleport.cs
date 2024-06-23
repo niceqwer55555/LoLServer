f54ff97686968309ab6463a6cb23895419fcce09
phillipchain <knights.phillip.chain@outlook.com>
2024-06-23 05:22:32 +0200

    [Add/Core] Teleport summoner spell

---
+++ Content/LeagueSandbox-Scripts/Characters/Global/SummonerTeleport.cs
@@ -0,0 +1,63 @@
+using static LeagueSandbox.GameServer.API.ApiFunctionManager;
+using LeagueSandbox.GameServer.Scripting.CSharp;
+using System.Numerics;
+using GameServerCore.Scripting.CSharp;
+using LeagueSandbox.GameServer.GameObjects.AttackableUnits;
+using LeagueSandbox.GameServer.GameObjects.AttackableUnits.AI;
+using LeagueSandbox.GameServer.GameObjects.SpellNS;
+using GameServerCore.Enums;
+
+namespace Spells
+{
+    public class SummonerTeleport : ISpellScript
+    {
+        public SpellScriptMetadata ScriptMetadata { get; private set; } = new SpellScriptMetadata()
+        {
+            NotSingleTargetSpell = true
+        };
+
+        public void OnActivate(ObjAIBase owner, Spell spell)
+        {
+        }
+
+        public void OnDeactivate(ObjAIBase owner, Spell spell)
+        {
+        }
+        static internal Vector2 endpos;
+        public void OnSpellPreCast(ObjAIBase owner, Spell spell, AttackableUnit target, Vector2 start, Vector2 end)
+        {
+            endpos = end;
+            //Global_Asc_teleport.troy
+            //Global_Asc_Teleport_reappear.troy
+            //global_asc_teleport_target.troy
+
+            //AddParticle(owner, null, "global_ss_teleport_blue.troy", owner.Position, lifetime: 4.0f);
+            AddBuff("TeleportBuff", 4.0f, 1, spell, owner, target as ObjAIBase);
+            //AddParticleTarget(owner, owner, "global_ss_flash_02.troy", owner);
+        }
+
+        public void OnSpellCast(Spell spell)
+        {
+        }
+
+        public void OnSpellPostCast(Spell spell)
+        {
+        }
+
+        public void OnSpellChannel(Spell spell)
+        {
+        }
+
+        public void OnSpellChannelCancel(Spell spell, ChannelingStopSource source)
+        {
+        }
+
+        public void OnSpellPostChannel(Spell spell)
+        {
+        }
+
+        public void OnUpdate(float diff)
+        {
+        }
+    }
+}
