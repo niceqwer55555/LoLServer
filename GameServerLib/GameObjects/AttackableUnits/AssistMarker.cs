aa1a86ce0350f8e1f765283adeda2df5dec14ae6
niceqwr55555 <niceqwr55555@example.com>
2025-07-29 19:23:14 +0800

    fix bug

--- GameServerLib/GameObjects/AttackableUnits/AssistMarker.cs
+++ GameServerLib/GameObjects/AttackableUnits/AssistMarker.cs
@@ -1,11 +1,12 @@
-﻿namespace LeagueSandbox.GameServer.GameObjects.AttackableUnits.AI;
-
-internal class AssistMarker
+﻿namespace LeagueSandbox.GameServer.GameObjects.AttackableUnits.AI
 {
-    internal ObjAIBase Source;
-    internal float StartTime;
-    internal float EndTime;
-    internal float PhysicalDamage;
-    internal float MagicalDamage;
-    internal float TrueDamage;
+  internal class AssistMarker
+    {
+        internal ObjAIBase Source;
+        internal float StartTime;
+        internal float EndTime;
+        internal float PhysicalDamage;
+        internal float MagicalDamage;
+        internal float TrueDamage;
+}
 }
--- GameServerLib/Handlers/ChampionDeathHandler.cs
+++ GameServerLib/Handlers/ChampionDeathHandler.cs
@@ -7,203 +7,204 @@ using System;
 using LeagueSandbox.GameServer.Handlers;
 using GameServerLib.GameObjects.AttackableUnits;
 
-namespace GameServerLib.Handlers;
-
-internal static class ChampionDeathHandler
+namespace GameServerLib.Handlers
 {
-    private const float EVIL_UNK_CONST_LEVEL_DIFF_BONUS_EXP_MULT = 0.1455f;
+    internal static class ChampionDeathHandler
+    {
+        private const float EVIL_UNK_CONST_LEVEL_DIFF_BONUS_EXP_MULT = 0.1455f;
 
-    private static Game _game;
-    internal static void Init(Game game)
-    {
-        _game = game;
-    }
-	
-    internal static OnDeathAssist OnDeathAssistConstructor(DeathData deathData, AssistMarker marker)
-    {
-        return new()
+        private static Game _game;
+        internal static void Init(Game game)
         {
-            AtTime = marker.StartTime,
-            PhysicalDamage = marker.PhysicalDamage,
-            MagicalDamage = marker.MagicalDamage,
-            TrueDamage = marker.TrueDamage,
-            OrginalGoldReward = deathData.GoldReward,
-            KillerNetID = deathData.Killer.NetId,
-            OtherNetID = deathData.Unit.NetId
-        };
-    }
-
-    internal static void NotifyAssistEvent(Dictionary<ObjAIBase, AssistMarker> assists, DeathData data)
-    {
-        float assistPercent = 1.0f / assists.Count;
-        foreach (var champion in assists.Keys)
-        {
-            OnDeathAssist onDeathAssist = OnDeathAssistConstructor(data, assists[champion]);
-            onDeathAssist.PercentageOfAssist = assistPercent;
-            _game.PacketNotifier.NotifyOnEvent(onDeathAssist, champion);
+            _game = game;
         }
-    }
 
-    internal static void NotifyDeathEvent(DeathData data, ObjAIBase[] assists = null)
-    {
-        OnChampionDie championDie = new()
+        internal static OnDeathAssist OnDeathAssistConstructor(DeathData deathData, AssistMarker marker)
         {
-            OtherNetID = data.Killer.NetId,
-            GoldGiven = data.GoldReward,
-            AssistCount = assists.Length,
-        };
-
-        if (assists != null)
-        {
-            for (int i = 0; i < assists.Length && i < 12; i++)
+            return new()
             {
-                championDie.Assists[i] = assists[i].NetId;
+                AtTime = marker.StartTime,
+                PhysicalDamage = marker.PhysicalDamage,
+                MagicalDamage = marker.MagicalDamage,
+                TrueDamage = marker.TrueDamage,
+                OrginalGoldReward = deathData.GoldReward,
+                KillerNetID = deathData.Killer.NetId,
+                OtherNetID = deathData.Unit.NetId
+            };
+        }
+
+        internal static void NotifyAssistEvent(Dictionary<ObjAIBase, AssistMarker> assists, DeathData data)
+        {
+            float assistPercent = 1.0f / assists.Count;
+            foreach (var champion in assists.Keys)
+            {
+                OnDeathAssist onDeathAssist = OnDeathAssistConstructor(data, assists[champion]);
+                onDeathAssist.PercentageOfAssist = assistPercent;
+                _game.PacketNotifier.NotifyOnEvent(onDeathAssist, champion);
             }
         }
 
-        _game.PacketNotifier.NotifyOnEvent(championDie, data.Unit);
-    }
-
-    internal static void ProcessKill(DeathData data)
-    {
-        MapScriptHandler map = _game.Map;
-
-        // TODO: Find out if it's possible to unhardcode some of the fractions used here.
-        Champion killed = data.Unit as Champion;
-        Champion killer = data.Killer as Champion;
-        data.GoldReward = map.MapScript.MapScriptMetadata.ChampionBaseGoldValue;
-        if (killed.ChampStats.CurrentKillingSpree > 1)
+        internal static void NotifyDeathEvent(DeathData data, ObjAIBase[] assists = null)
         {
-            data.GoldReward = Math.Min(data.GoldReward * MathF.Pow(7f / 6f, killed.ChampStats.CurrentKillingSpree - 1), map.MapScript.MapScriptMetadata.ChampionMaxGoldValue);
-        }
-        else if (killed.ChampStats.CurrentKillingSpree == 0 && killed.DeathSpree >= 1)
-        {
-            data.GoldReward *= 11f / 12f;
-            if (killed.DeathSpree > 1)
+            OnChampionDie championDie = new()
             {
-                data.GoldReward = Math.Max(data.GoldReward * MathF.Pow(0.8f, killed.DeathSpree / 2), map.MapScript.MapScriptMetadata.ChampionMinGoldValue);
+                OtherNetID = data.Killer.NetId,
+                GoldGiven = data.GoldReward,
+                AssistCount = assists.Length,
+            };
+
+            if (assists != null)
+            {
+                for (int i = 0; i < assists.Length && i < 12; i++)
+                {
+                    championDie.Assists[i] = assists[i].NetId;
+                }
             }
+
+            _game.PacketNotifier.NotifyOnEvent(championDie, data.Unit);
+        }
+
+        internal static void ProcessKill(DeathData data)
+        {
+            MapScriptHandler map = _game.Map;
+
+            // TODO: Find out if it's possible to unhardcode some of the fractions used here.
+            Champion killed = data.Unit as Champion;
+            Champion killer = data.Killer as Champion;
+            data.GoldReward = map.MapScript.MapScriptMetadata.ChampionBaseGoldValue;
+            if (killed.ChampStats.CurrentKillingSpree > 1)
+            {
+                data.GoldReward = Math.Min(data.GoldReward * MathF.Pow(7f / 6f, killed.ChampStats.CurrentKillingSpree - 1), map.MapScript.MapScriptMetadata.ChampionMaxGoldValue);
+            }
+            else if (killed.ChampStats.CurrentKillingSpree == 0 && killed.DeathSpree >= 1)
+            {
+                data.GoldReward *= 11f / 12f;
+                if (killed.DeathSpree > 1)
+                {
+                    data.GoldReward = Math.Max(data.GoldReward * MathF.Pow(0.8f, killed.DeathSpree / 2), map.MapScript.MapScriptMetadata.ChampionMinGoldValue);
+                }
+                killed.DeathSpree++;
+            }
+
+            if (!map.MapScript.HasFirstBloodHappened)
+            {
+                data.GoldReward += map.MapScript.MapScriptMetadata.FirstBloodExtraGold;
+                map.MapScript.HasFirstBloodHappened = true;
+            }
+
+            Dictionary<ObjAIBase, AssistMarker> assists = new();
+            foreach (var assistMarker in killed.EnemyAssistMarkers)
+            {
+                if (!assists.ContainsKey(assistMarker.Source) && assistMarker.Source is Champion c)
+                {
+                    assists.Add(c, assistMarker);
+                    RecursiveGetAlliedAssists(assists, c, data);
+                }
+            }
+            assists.Remove(killer);
+            assists = assists.OrderBy(x => x.Value.StartTime).ToDictionary(x => x.Key, x => x.Value);
+            ObjAIBase[] assistObjArray = assists.Keys.ToArray();
+
+            NotifyAssistEvent(assists, data);
+            NotifyDeathEvent(data, assistObjArray);
+            NotifyChampionKillEvent(data);
+            ProcessKillRewards(killed, killer, assistObjArray, data.GoldReward);
+            UpdateKillerStats(killer);
+            killed.KillSpree = 0;
             killed.DeathSpree++;
         }
 
-        if (!map.MapScript.HasFirstBloodHappened)
+        internal static void NotifyChampionKillEvent(DeathData data)
         {
-            data.GoldReward += map.MapScript.MapScriptMetadata.FirstBloodExtraGold;
-            map.MapScript.HasFirstBloodHappened = true;
+            _game.PacketNotifier.NotifyOnEvent(new OnChampionKill() { OtherNetID = data.Unit.NetId }, data.Killer);
         }
 
-        Dictionary<ObjAIBase, AssistMarker> assists = new();
-        foreach (var assistMarker in killed.EnemyAssistMarkers)
+        internal static void UpdateKillerStats(Champion c)
         {
-            if (!assists.ContainsKey(assistMarker.Source) && assistMarker.Source is Champion c)
+            //Check if GoldFromMinions should be reset
+            c.GoldFromMinions = 0;
+            c.ChampStats.Kills++;
+            c.DeathSpree = 0;
+            c.KillSpree++;
+            if (c.KillSpree > c.ChampStats.LargestKillingSpree)
             {
-                assists.Add(c, assistMarker);
-                RecursiveGetAlliedAssists(assists, c, data);
-            }
-        }
-        assists.Remove(killer);
-        assists = assists.OrderBy(x => x.Value.StartTime).ToDictionary(x => x.Key, x => x.Value);
-        ObjAIBase[] assistObjArray = assists.Keys.ToArray();
-
-        NotifyAssistEvent(assists, data);
-        NotifyDeathEvent(data, assistObjArray);
-        NotifyChampionKillEvent(data);
-        ProcessKillRewards(killed, killer, assistObjArray, data.GoldReward);
-        UpdateKillerStats(killer);
-        killed.KillSpree = 0;
-        killed.DeathSpree++;
-    }
-
-    internal static void NotifyChampionKillEvent(DeathData data)
-    {
-        _game.PacketNotifier.NotifyOnEvent(new OnChampionKill() { OtherNetID = data.Unit.NetId }, data.Killer);
-    }
-
-    internal static void UpdateKillerStats(Champion c)
-    {
-        //Check if GoldFromMinions should be reset
-        c.GoldFromMinions = 0;
-        c.ChampStats.Kills++;
-        c.DeathSpree = 0;
-        c.KillSpree++;
-        if (c.KillSpree > c.ChampStats.LargestKillingSpree)
-        {
-            c.ChampStats.LargestKillingSpree = c.KillSpree;
-        }
-    }
-
-    internal static void RecursiveGetAlliedAssists(Dictionary<ObjAIBase, AssistMarker> assistMarkers, Champion champ, DeathData deathData)
-    {
-        foreach (var assist in champ.AlliedAssistMarkers)
-        {
-            if (assist.Source is not Champion c)
-            {
-                continue;
-            }
-
-            if (!assistMarkers.ContainsKey(assist.Source))
-            {
-                assistMarkers.Add(assist.Source, assist);
-                RecursiveGetAlliedAssists(assistMarkers, c, deathData);
-            }
-            else
-            {
-                assistMarkers[c].StartTime = assistMarkers[c].StartTime < assist.StartTime ? assist.StartTime : assistMarkers[c].StartTime;
-            }
-        }
-    }
-
-    internal static void ProcessKillRewards(Champion killed, Champion killer, ObjAIBase[] assists, float gold)
-    {
-        float xpShareFactor = assists.Length + 1;
-
-        killer.AddExperience(GetEXPGrantedFromChampion(killer, killed) / xpShareFactor);
-        foreach (ObjAIBase obj in assists)
-        {
-            if (obj is Champion c)
-            {
-                c.AddExperience(GetEXPGrantedFromChampion(c, killed) / xpShareFactor);
+                c.ChampStats.LargestKillingSpree = c.KillSpree;
             }
         }
 
-        killer.AddGold(killer, gold);
-        foreach (ObjAIBase obj in assists)
+        internal static void RecursiveGetAlliedAssists(Dictionary<ObjAIBase, AssistMarker> assistMarkers, Champion champ, DeathData deathData)
         {
-            if (obj is not Champion c)
+            foreach (var assist in champ.AlliedAssistMarkers)
             {
-                continue;
+                if (assist.Source is not Champion c)
+                {
+                    continue;
+                }
+
+                if (!assistMarkers.ContainsKey(assist.Source))
+                {
+                    assistMarkers.Add(assist.Source, assist);
+                    RecursiveGetAlliedAssists(assistMarkers, c, deathData);
+                }
+                else
+                {
+                    assistMarkers[c].StartTime = assistMarkers[c].StartTime < assist.StartTime ? assist.StartTime : assistMarkers[c].StartTime;
+                }
+            }
+        }
+
+        internal static void ProcessKillRewards(Champion killed, Champion killer, ObjAIBase[] assists, float gold)
+        {
+            float xpShareFactor = assists.Length + 1;
+
+            killer.AddExperience(GetEXPGrantedFromChampion(killer, killed) / xpShareFactor);
+            foreach (ObjAIBase obj in assists)
+            {
+                if (obj is Champion c)
+                {
+                    c.AddExperience(GetEXPGrantedFromChampion(c, killed) / xpShareFactor);
+                }
             }
 
-            float assistGold = gold / 2 * (1.0f / assists.Length);
-            int killDiff = c.ChampStats.Assists - c.ChampStats.Kills;
-            if (killDiff > 0)
+            killer.AddGold(killer, gold);
+            foreach (ObjAIBase obj in assists)
             {
-                assistGold += 15 + 15 * MathF.Min(killDiff, 3);
+                if (obj is not Champion c)
+                {
+                    continue;
+                }
+
+                float assistGold = gold / 2 * (1.0f / assists.Length);
+                int killDiff = c.ChampStats.Assists - c.ChampStats.Kills;
+                if (killDiff > 0)
+                {
+                    assistGold += 15 + 15 * MathF.Min(killDiff, 3);
+                }
+                //TODO: Check if the Assist + Bonus can't exeed the original value gained by the killer or just the Bonus alone.
+                assistGold = MathF.Min(gold, assistGold);
+
+                c.AddGold(c, assistGold);
+                c.ChampStats.Assists++;
             }
-            //TODO: Check if the Assist + Bonus can't exeed the original value gained by the killer or just the Bonus alone.
-            assistGold = MathF.Min(gold, assistGold);
-
-            c.AddGold(c, assistGold);
-            c.ChampStats.Assists++;
         }
-    }
 
-    public static float GetEXPGrantedFromChampion(Champion killer, Champion killed)
-    {
-        int cLevel = killed.Stats.Level;
-        float EXP = (_game.Map.MapData.ExpCurve[cLevel] - _game.Map.MapData.ExpCurve[cLevel - 1]) * _game.Map.MapData.BaseExpMultiple;
-
-        float levelDifference = cLevel - killer.Stats.Level;
-
-        if (cLevel < 0)
+        public static float GetEXPGrantedFromChampion(Champion killer, Champion killed)
         {
-            EXP -= EXP * MathF.Min(_game.Map.MapData.LevelDifferenceExpMultiple * levelDifference, _game.Map.MapData.MinimumExpMultiple);
-        }
-        else if (cLevel > 0)
-        {
-            EXP += EXP * (levelDifference * EVIL_UNK_CONST_LEVEL_DIFF_BONUS_EXP_MULT);
-        }
+            int cLevel = killed.Stats.Level;
+            float EXP = (_game.Map.MapData.ExpCurve[cLevel] - _game.Map.MapData.ExpCurve[cLevel - 1]) * _game.Map.MapData.BaseExpMultiple;
 
-        return EXP;
+            float levelDifference = cLevel - killer.Stats.Level;
+
+            if (cLevel < 0)
+            {
+                EXP -= EXP * MathF.Min(_game.Map.MapData.LevelDifferenceExpMultiple * levelDifference, _game.Map.MapData.MinimumExpMultiple);
+            }
+            else if (cLevel > 0)
+            {
+                EXP += EXP * (levelDifference * EVIL_UNK_CONST_LEVEL_DIFF_BONUS_EXP_MULT);
+            }
+
+            return EXP;
+        }
     }
 }
